06fb16cc21452b0600d6146cc1eaee4aac1c6f4e
Cores luo <944855832@qq.com>
2023-05-15 18:22:58 +0800

    Update tarFile.sh

--- tarFile.sh
+++ tarFile.sh
@@ -6,9 +6,10 @@ ip_addr=$(ip addr | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut 
 # 打包当前目录下的所有文件到log_x.tar.gz
 tar -czf "log_${ip_addr}.tar.gz" .
 
-# 启动Python HTTP服务器
-python3 -m http.server 11111 &
-
 # 打印提示信息
 echo "已将当前目录下的文件打包到 log_${ip_addr}.tar.gz 文件中。"
 echo "现在可以在浏览器中访问 http://${ip_addr}:11111/log_${ip_addr}.tar.gz 来下载打包的文件。"
+echo "启动Http服务"
+# 启动Python HTTP服务器
+python3 -m http.server 11111 
+
